d19a1e8e6567400617fd45f511120a32d5375a90
Tectum Engineering <admin@TectumEngineering.onmicrosoft.com>
2026-08-26 13:39:58 +0500

    fix(admin): add missing escapeHtml helper in admin.js for audit log renderer

--- static/js/admin.js
+++ static/js/admin.js
@@ -565,6 +565,16 @@ async function importPlanBoard() {
     }
 }
 
+function escapeHtml(str) {
+    if (str === null || str === undefined) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
 let adminAuditLogsCached = [];
 let currentAuditModuleFilter = 'all';
 let currentAuditDatePreset = 'all';
